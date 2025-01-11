182d87e9df3d525633aaf1ef023fddc08fbdd511
Nick Pseudo <tomdop.dakar@gmail.com>
2025-01-11 04:25:53 +0100

    C1L11

--- main.go
+++ main.go
@@ -4,13 +4,20 @@ import (
 	//"fmt"
 	"log"
 	"net/http"
+	"io"
 )
 
 func main() {
 	multiPlex := http.NewServeMux()
 	normDir := http.Dir("./")
 	fileServer := http.FileServer(normDir)
-	multiPlex.Handle("/", fileServer)
+	strippedFileServer := http.StripPrefix("/app", fileServer)
+	multiPlex.Handle("/", strippedFileServer)
+	multiPlex.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "OK")
+	})
 	//fmt.Printf("%T",multiPlex)
 	s := &http.Server{
 		Addr:           ":8080",
